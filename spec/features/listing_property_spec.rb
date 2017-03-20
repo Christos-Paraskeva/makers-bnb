2ca4cf9ff3e0526b24dd7743d5e82e07f55f63f3
Christos <chris.paraskeva10@gmail.com>
2017-03-20 15:53:17 +0000

    first failing feature test written

---
+++ spec/features/listing_property_spec.rb
@@ -0,0 +1,15 @@
+# require 'spec_helper'
+
+feature 'As a user' do
+  scenario 'I can list a property' do
+    visit ('/property')
+    click_button('List Property')
+    expect(current_path).to eq '/property/new'
+
+    within '/property/new' do
+      expect(page).to have_content('Name')
+      expect(page).to have_content('Description')
+      expect(page).to have_content('Price Per Night')
+    end
+  end
+end
