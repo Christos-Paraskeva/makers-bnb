9c2e1cf3407567bffc4f195426c1ee7d47b0309d
Alex Imm <alexanderimm@hotmail.co.uk>
2017-03-20 15:58:51 +0000

    creating sign_up branch

---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,16 @@
+# As a user,
+# So that I can use the MakersBnB,
+# I can sign up with a name, an email address and a password.
+
+feature 'Signing up' do
+
+  scenario 'user can sign up' do
+    visit '/'
+    click_button 'Sign up'
+    fill_in 'Name', with: 'Alex'
+    fill_in 'Email', with: 'aleximm@immsinns.com'
+    fill_in 'Password', with: 'secret123'
+    click_button 'Submit'
+    expect(page).to have_content 'Welcome Alex!'
+  end
+end
