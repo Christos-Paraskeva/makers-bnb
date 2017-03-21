ac0c542b6a45fd142feee1d12951e44450e3566e
Christos <chris.paraskeva10@gmail.com>
2017-03-21 15:46:12 +0000

    add bcrypt for password encryption

--- app/app.rb
+++ app/app.rb
@@ -4,64 +4,68 @@ require 'sinatra'
 require_relative 'database_setup'
 require_relative 'models/user'
 require_relative 'models/property'
+require 'bcrypt'
 
 class MakersBNB < Sinatra::Base
   enable :sessions
 
-get '/' do
-  'Welcome!'
-  redirect '/log_in'
-end
+  include BCrypt
 
-get '/log_in' do
-  erb :'user/log_in'
-end
-
-post '/log_in' do
-  session[:user] = User.first(email: params[:email])
-  if (session[:user] == nil) || (session[:user].password != params[:password])
-    raise 'Incorrect Login Details'
+  get '/' do
+    'Welcome!'
+    redirect '/log_in'
   end
-  redirect '/property'
-end
 
-get '/sign_up' do
-  erb :'user/sign_up'
-end
+  get '/log_in' do
+    erb :'user/log_in'
+  end
 
-post '/sign_up' do
-  User.create(name:        params[:name],
-              email:       params[:email],
-              password:    params[:password])
-  user = User.first(email:  params[:email])
-  session[:user] = user
-  redirect to('/property')
-end
+  post '/log_in' do
+    session[:user] = User.first(email: params[:email])
+    if (session[:user] == nil) || (User.check_password(params[:email], params[:password]))
+      raise 'Incorrect Login Details'
+    end
+    redirect '/property'
+  end
 
-get '/property' do
-  @property = Property.all
-  @user = session[:user]
-  erb :'links/property'
-end
+  get '/sign_up' do
+    erb :'user/sign_up'
+  end
 
-post '/property' do
-  Property.create(title: params[:title],
-                  description: params[:description],
-                  price_per_night: params[:price_per_night],
-                  location: params[:location],
-                  available: params[:available])
-  redirect '/property'
-end
+  post '/sign_up' do
+    password_digest = Password.create(params[:password])
+    User.create(name:        params[:name],
+                email:       params[:email],
+                password_digest:  password_digest)
+    user = User.first(email: params[:email])
+    session[:user] = user
+    redirect to('/property')
+  end
 
-get '/property/new' do
-  erb :'links/listing_property'
-end
+  get '/property' do
+    @property = Property.all
+    @user = session[:user]
+    erb :'links/property'
+  end
 
-post '/log_out' do
-  session[:user] = nil
-  redirect '/'
-end
+  post '/property' do
+    Property.create(title: params[:title],
+                    description: params[:description],
+                    price_per_night: params[:price_per_night],
+                    location: params[:location],
+                    available: params[:available])
+    redirect '/property'
+  end
 
-run! if app_file == $0
+  get '/property/new' do
+    erb :'links/listing_property'
+  end
+
+  post '/log_out' do
+    session[:user] = nil
+    redirect '/'
+  end
+
+  run! if app_file == $0
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,14 @@ class User
   property :id,        Serial
   property :name,      String
   property :email,     String
-  property :password,  Text
+  property :password_digest,  Text
+
+  def self.check_password(email, password)
+    user = first(email: email)
+    if (user && BCrypt::Password.new(user.password_digest)) == password
+      false
+    else
+      true
+    end
+  end
 end
