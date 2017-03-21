788e3187ec2566be2d4a9d1c47f0802c672cee03
Eva Balog <evebalog@netscape.net>
2017-03-21 13:13:22 +0000

    Added failing feature test for logging out user

--- spec/features/log_out_spec.rb
+++ spec/features/log_out_spec.rb
@@ -1,3 +1,12 @@
 # As a user,
 # So that only I can use my account,
 # I can log out.
+
+feature 'As a user' do
+  scenario 'I can log out' do
+    visit '/'
+    user_sign_up
+    click_button('Log Out')
+    expect(current_path).not_to have_content("Welcome Alex")
+  end
+end
