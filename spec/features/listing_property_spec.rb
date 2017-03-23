d1c586329766b04607bdf8237f5c0c4e99ce26d5
Clem Capel-Bird <clemcapelbird@gmail.com>
2017-03-23 15:05:40 +0000

    Correcting typo from merge

--- spec/features/listing_property_spec.rb
+++ spec/features/listing_property_spec.rb
@@ -12,7 +12,7 @@ feature 'As a user' do
   scenario 'I can list a property with an image' do
     visit '/'
     user_sign_up
-    click_button('List Property')
+    click_link('List Property')
     expect(current_path).to eq '/property/new'
     list_a_new_property
     expect(page).to have_xpath("//img[@src='http://www.bigpicturebigsound.com/artman2/uploads/1/star-wars-home-theater.jpg']")
