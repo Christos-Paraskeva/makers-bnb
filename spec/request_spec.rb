f364741d817f2e8a882e979c967299a63ec9b5d3
Eva Balog <evebalog@netscape.net>
2017-03-23 18:39:49 +0000

    Trying to validate request

--- spec/request_spec.rb
+++ spec/request_spec.rb
@@ -1,7 +1,7 @@
 require './app/models/request'
 
 describe Request do
-  subject(:request) { described_class.new }
+
 # let(:property) { double('property', :title => "Jabba's hut",
 #                     :description     => "dark",
 #                     :price_per_night => 30,
@@ -13,15 +13,15 @@ describe Request do
 
   describe "Valid request" do
     it "returns true if requested start date is before end date" do
-      request.start_date = Date.new(2017, 12, 10)
-      request.end_date = Date.new(2017, 12, 14)
-      expect(request.valid_request?).to equal(true)
+      subject.start_date = Date.new(2017, 12, 10)
+      subject.end_date = Date.new(2017, 12, 14)
+      expect(subject.valid_request?).to equal(true)
     end
 
     it "returns false if requested end date is after start date" do
-      request.start_date = Date.new(2017, 12, 14)
-      request.end_date = Date.new(2017, 12, 10)
-      expect(request.valid_request?).to equal(false)
+      subject.start_date = Date.new(2017, 12, 14)
+      subject.end_date = Date.new(2017, 12, 10)
+      expect(subject.valid_request?).to equal(false)
     end
   end
 end
