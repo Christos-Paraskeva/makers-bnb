0c288878dc5145f7ea22893a47c92054fd9c8655
Alex Imm <alexanderimm@hotmail.co.uk>
2017-03-24 11:51:14 +0000

    removed commented code

--- app/models/request.rb
+++ app/models/request.rb
@@ -13,17 +13,6 @@ class Request
   belongs_to :user
   belongs_to :property
 
-  # def initialize(params)
-  #     request = Request.new(start_date:             params[:start_date],
-  #                            end_date:               params[:end_date],
-  #                            confirmation_status:    false,
-  #                            user_id:                session[:user].id,
-  #                            property_id:            session[:property_id])
-  #   self.valid_request?
-  # end
-
-# private
-
   def valid_request?
     self.start_date <= self.end_date ? true : false
   end
--- spec/request_spec.rb
+++ spec/request_spec.rb
@@ -2,15 +2,6 @@ require './app/models/request'
 
 describe Request do
 
-# let(:property) { double('property', :title => "Jabba's hut",
-#                     :description     => "dark",
-#                     :price_per_night => 30,
-#                     :location        => "Tatooine",
-#                     :available_from  => "12-12-2017",
-#                     :available_to    => "16-12-2017",
-#                     :image_url       => "double_url",
-#                     :user_id         => "1")
-
   describe "Valid request" do
     it "returns true if requested start date is before end date" do
       subject.start_date = Date.new(2017, 12, 10)
