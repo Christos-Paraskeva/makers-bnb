b52533a0d9d0bbab45b102330bcce1cccf94edff
Clem Capel-Bird <clemcapelbird@gmail.com>
2017-03-24 11:19:33 +0000

    WIP tests failing. Creating second user signup for deeper testing

--- spec/features/profile_page_spec.rb
+++ spec/features/profile_page_spec.rb
@@ -4,7 +4,29 @@ feature 'Property owners can visit their profile page' do
     user_sign_up
     click_link('List Property')
     list_a_new_property
+    # click_button('Log Out')
+    # second_property_owner_list_property
     click_link('My Profile')
     expect(page).to have_content("Imm's Inn")
+
+    # expect(page).to have_content("Chris' Pad")
+  end
+
+  scenario 'And then can return to view all properties' do
+    visit '/'
+    user_sign_up
+    click_link('List Property')
+    list_a_new_property
+    click_link('My Profile')
+    # click_link('View Properties')
+    expect(page).to have_content("Imm's Inn")
+  end
+end
+
+describe "We're testing this", focus: true do
+  scenario 'Hello' do
+    visit '/'
+    second_property_owner_list_property
+    expect(page).to have_content "Chris' Pad"
   end
 end
